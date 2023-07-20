958cd0ae138a4d434f05ce9e3fa59a73934d45b3
ALEX NG'ANG'A WAWERU <ngashalex8@gmail.com>
2023-07-20 01:08:23 -0700

    Update schema.sql
    Add missing files

--- schema.sql
+++ schema.sql
@@ -9,3 +9,27 @@ CREATE TABLE animals (
 	weight_kg  decimal,
 	species varchar(50)
 );
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INTEGER
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER REFERENCES species (id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER REFERENCES owners (id);
+
+
+
+SELECT * FROM animals;
