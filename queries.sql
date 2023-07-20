e9d2524056b04d1f1d0a48aff629544e78cfbc39
Waweru <ngashalex8@gmail.com>
2023-07-19 23:04:43 -0700

    Add Multiple tables queries

--- queries.sql
+++ queries.sql
@@ -72,6 +72,45 @@ FROM animals
 WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 1990 AND 2000
 GROUP BY species;
 
+
+SELECT a.name AS animal_name
+FROM animals a
+JOIN owners o ON a.owner_id = o.id
+WHERE o.full_name = 'Melody Pond';
+
+
+SELECT a.name AS animal_name
+FROM animals a
+JOIN species s ON a.species_id = s.id
+WHERE s.name = 'Pokemon';
+
+SELECT o.full_name AS owner_name, COALESCE(a.name, 'None') AS animal_name
+FROM owners o
+LEFT JOIN animals a ON o.id = a.owner_id;
+
+SELECT s.name AS species_name, COUNT(*) AS total_animals
+FROM animals a
+JOIN species s ON a.species_id = s.id
+GROUP BY s.name;
+
+SELECT a.name AS animal_name
+FROM animals a
+JOIN species s ON a.species_id = s.id
+JOIN owners o ON a.owner_id = o.id
+WHERE s.name = 'Digimon' AND o.full_name = 'Jennifer Orwell';
+
+SELECT a.name AS animal_name
+FROM animals a
+JOIN owners o ON a.owner_id = o.id
+WHERE o.full_name = 'Dean Winchester' AND a.escape_attempts = 0;
+
+SELECT o.full_name AS owner_name, COUNT(*) AS total_animals
+FROM owners o
+LEFT JOIN animals a ON o.id = a.owner_id
+GROUP BY o.full_name
+ORDER BY COUNT(*) DESC
+LIMIT 1;
+
 SELECT * FROM animals;
 
 
